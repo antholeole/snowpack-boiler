3f4d37dda4bd3b1588fcc856c6ed7911314f3834
antholeole <antholeinik@gmail.com>
2020-11-21 13:54:55 -0600

    inital commit

---
+++ snowpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-sass'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const App = (): JSX.Element => {
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
